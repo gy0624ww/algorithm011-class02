86548b65ebc128d8e248552e50e27aca968259df
guanyue <guanyue@360.cn>
2020-07-12 23:00:32 +0800

    week 3

---
+++ Week_03/105. 从前序与中序遍历序列构造二叉树.go
@@ -0,0 +1,18 @@
+package Week_03
+
+// 前序遍历 root (index=0) 寻找 中序遍历 root (index=k)
+
+func buildTree(preorder []int, inorder []int) *TreeNode {
+	for k := range inorder {
+		if inorder[k] == preorder[0] {//中序遍历 root (index=k)
+			return &TreeNode{
+				Val: preorder[0],
+				//Val: inorder[root],
+				Left:  buildTree(preorder[1:k+1], inorder[0:k]),
+				Right: buildTree(preorder[k+1:], inorder[k+1:]),
+			}
+		}
+	}
+	return nil
+}
+
--- Week_03/README.md
+++ Week_03/README.md
@@ -1,1 +1,23 @@
 学习笔记
+
+抵制人肉递归
+找最近重复性，找到最近最简方法，将其拆解成可重复解决的问题（重复子问题）
+数学归纳法思维（枚举，n=1怎么样 n=2怎么样，不要过多，前几层即可要不就会变成人肉递归，最后泛化成n=n怎么样）
+
+代码模板：
+
+```
+public void recur(int level, int param) {
+    // 终止条件
+    if (level > MAX_LEVEL) {
+        // 处理结果
+        return;
+    }
+    // 处理当前层逻辑
+    process(level, param);
+
+    // 进入下一层
+    recur(level:level+1, newParam);
+    // 一些当前层需要清理的东西
+}
+```
